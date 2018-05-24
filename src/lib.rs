26186597a0f92c441b9b637a550d111ddd604db8
Jonas Bushart <jonas@bushart.org>
2018-05-24 22:38:29 +0200

    Add deserialize for chrono::DateTime<Utc>
    Serialize from a timestamp in a flexible format.
    Supports int, float, string

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,16 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
+#![deny(
+    missing_debug_implementations, missing_copy_implementations, trivial_casts,
+    trivial_numeric_casts, unused_extern_crates, unused_import_braces, unused_qualifications,
+    variant_size_differences
+)]
+#![allow(missing_docs)]
+
+#[cfg(feature = "chrono")]
+extern crate chrono as chrono_crate;
+#[cfg(feature = "chrono")]
+extern crate serde;
+#[cfg(feature = "json")]
+extern crate serde_json;
+
+#[cfg(feature = "chrono")]
+pub mod chrono;
