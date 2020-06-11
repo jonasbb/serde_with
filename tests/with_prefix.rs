e4347d01c77a2626e44b4b7e0f6de52ec3218248
Jonas Bushart <jonas@bushart.org>
2020-06-11 21:02:27 +0200

    Add test case for with_prefix with a unit variant enum

--- tests/with_prefix.rs
+++ tests/with_prefix.rs
@@ -122,3 +122,47 @@ fn test_plain_with_prefix() {
 
     assert_eq!(m, serde_json::from_str(&j).unwrap());
 }
+
+/// Ensure that with_prefix works for unit type enum variants.
+#[test]
+fn test_enum_unit_variant_with_prefix() {
+    use std::collections::BTreeMap;
+
+    #[derive(Hash, PartialEq, Eq, Debug, Serialize, Deserialize, Ord, PartialOrd)]
+    enum Foo {
+        One,
+        Two,
+        Three,
+    }
+
+    #[derive(Hash, PartialEq, Eq, Debug, Serialize, Deserialize, Ord, PartialOrd)]
+    struct Data {
+        stuff: String,
+
+        #[serde(flatten, with = "foo")]
+        foo: BTreeMap<Foo, i32>,
+    }
+    with_prefix!(foo "foo_");
+
+    let mut my_foo = BTreeMap::new();
+    my_foo.insert(Foo::One, 1);
+    my_foo.insert(Foo::Two, 2);
+    my_foo.insert(Foo::Three, 3);
+
+    let d = Data {
+        stuff: "Stuff".to_owned(),
+        foo: my_foo,
+    };
+
+    let expected = json!({
+        "stuff": "Stuff",
+        "foo_One": 1,
+        "foo_Two": 2,
+        "foo_Three": 3,
+    });
+
+    let j = serde_json::to_string(&d).unwrap();
+    assert_eq!(j, expected.to_string());
+
+    assert_eq!(d, serde_json::from_str(&j).unwrap());
+}
