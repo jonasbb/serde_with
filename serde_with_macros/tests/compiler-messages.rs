493274437a80c576badc9aac4a578aeb8088b530
Jonas Bushart <jonas@bushart.org>
2020-06-24 00:40:16 +0200

    Fix tarpaulin workaround for tarpauling 0.13.4+

--- serde_with_macros/tests/compiler-messages.rs
+++ serde_with_macros/tests/compiler-messages.rs
@@ -1,14 +1,10 @@
 // This test fails for older compiler versions since the error messages are different.
 #[rustversion::attr(before(1.38), ignore)]
+#[cfg_attr(tarpaulin, ignore)]
 // TODO The error messages are more detailed on nightly, thus breaking the test.
 #[rustversion::attr(nightly, ignore)]
 #[test]
 fn compile_test() {
-    // This test does not work under tarpaulin, so skip it if detected
-    if std::env::var("TARPAULIN") == Ok("1".to_string()) {
-        return;
-    }
-
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/compile-fail/*.rs");
 }
