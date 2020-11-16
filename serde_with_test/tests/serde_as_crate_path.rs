752b4808148e1ad59db42b541b189e7e7c31a12e
Jonas Bushart <jonas@bushart.org>
2020-11-16 22:12:43 +0100

    Extend serde_as_crate_path.rs with more wrong names

--- serde_with_test/tests/serde_as_crate_path.rs
+++ serde_with_test/tests/serde_as_crate_path.rs
@@ -1,8 +1,22 @@
 //! Test that the `serde_as(crate = "...")` argument allows compilation when the crate isn't
 //! available at path `::serde_with`
 
+#![allow(dead_code)]
+
 use s::{Deserialize, Serialize};
 use s_with::serde_as;
+
+// Ensure that types from the environment do not infect the macro
+#[allow(unused_imports)]
+use crate::Option::*;
+mod std {}
+type Result = ();
+enum Option {
+    Some,
+    None(()),
+    Ok,
+    Err,
+}
 
 #[serde(crate = "s")]
 #[serde_as(crate = "s_with")]
