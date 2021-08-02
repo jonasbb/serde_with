9f70c02252cdbc2047fbec0628dccdd67a442ee8
Jonas Bushart <jonas@bushart.org>
2021-08-02 20:32:23 +0200

    Fix trybuild tests for updated error messages of Rust 1.54

--- serde_with_macros/tests/compiler-messages.rs
+++ serde_with_macros/tests/compiler-messages.rs
@@ -1,5 +1,5 @@
 // This test fails for older compiler versions since the error messages are different.
-#[rustversion::attr(before(1.43), ignore)]
+#[rustversion::attr(before(1.54), ignore)]
 #[cfg_attr(tarpaulin, ignore)]
 // The error messages are different on beta and nightly, thus breaking the test.
 #[rustversion::attr(beta, ignore)]
