255cbeaa1e85e37b01d50a491aa364f490883929
Jonas Bushart <jonas@bushart.org>
2021-09-27 20:15:31 +0300

    Add size_hint functions required by the deserialization code
    The code is licences as "MIT OR Apache-2.0" and developed by
    * Erick Tryzelaar <erick.tryzelaar@gmail.com>
    * David Tolnay <dtolnay@gmail.com>

--- serde_content/src/lib.rs
+++ serde_content/src/lib.rs
@@ -34,3 +34,24 @@ extern crate serde;
 
 pub mod de;
 pub mod ser;
+
+mod size_hint {
+    pub(crate) fn from_bounds<I>(iter: &I) -> Option<usize>
+    where
+        I: Iterator,
+    {
+        helper(iter.size_hint())
+    }
+
+    #[inline]
+    pub(crate) fn cautious(hint: Option<usize>) -> usize {
+        std::cmp::min(hint.unwrap_or(0), 4096)
+    }
+
+    fn helper(bounds: (usize, Option<usize>)) -> Option<usize> {
+        match bounds {
+            (lower, Some(upper)) if lower == upper => Some(upper),
+            _ => None,
+        }
+    }
+}
