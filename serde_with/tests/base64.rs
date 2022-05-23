ade8109129a5a90b988050caac44b1a379876408
Jonas Bushart <jonas@bushart.org>
2022-05-23 13:13:20 +0000

    Ignore faulty clippy lint in tests
    https://github.com/rust-lang/rust-clippy/issues/8875

--- serde_with/tests/base64.rs
+++ serde_with/tests/base64.rs
@@ -1,5 +1,12 @@
-// This allows the tests to be written more uniform and not have to special case the last clone().
-#![allow(clippy::redundant_clone)]
+#![allow(
+    // clippy is broken and shows wrong warnings
+    // clippy on stable does not know yet about the lint name
+    unknown_lints,
+    // https://github.com/rust-lang/rust-clippy/issues/8867
+    clippy::derive_partial_eq_without_eq,
+    // This allows the tests to be written more uniform and not have to special case the last clone().
+    clippy::redundant_clone,
+)]
 
 mod utils;
 
