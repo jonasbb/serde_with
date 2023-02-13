6aa0db45df77b63d9f9cdeb307f0cd3cdbfe7696
Jonas Bushart <jonas@bushart.org>
2023-02-13 21:19:57 +0100

    Document why `double_option` cannot be used in `serde_as`
    And remove an outdated version check.

--- serde_with_macros/tests/compiler-messages.rs
+++ serde_with_macros/tests/compiler-messages.rs
@@ -1,5 +1,3 @@
-// This test fails for older compiler versions since the error messages are different.
-#[rustversion::attr(before(1.54), ignore)]
 #[cfg_attr(tarpaulin, ignore)]
 // The error messages are different on beta and nightly, thus breaking the test.
 #[rustversion::attr(beta, ignore)]
