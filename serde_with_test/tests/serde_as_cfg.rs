91630e7b8dbfa417c0b6f018575c14df0c8507ff
Jonas Bushart <jonas@bushart.org>
2023-07-03 14:18:29 +0200

    Add a test showing how cfg_eval can be used to gate serde_with attributes
    So far the story for cfg-gating serde_with attributes has not been
    great, as shown in #331 or #355.
    This adds a test showing how cfg_eval can be used to gate serde_with.
    
    Part of #607

---
+++ serde_with_test/tests/serde_as_cfg.rs
@@ -0,0 +1,24 @@
+//! Test that cfg_eval helps in cfg-gating serde_with attributes
+
+// Ensure no prelude is available
+#![no_implicit_prelude]
+#![allow(dead_code, unused_imports)]
+
+#[cfg_attr(test, ::cfg_eval::cfg_eval, ::s_with::serde_as(crate = "::s_with"))]
+#[cfg_attr(test, derive(::s::Serialize, ::s::Deserialize))]
+#[derive(Debug)]
+#[serde(crate = "::s")]
+struct S {
+    #[cfg_attr(test, serde_as(as = "::s_with::DisplayFromStr"))]
+    int: i32,
+}
+
+#[test]
+fn serde_as_cfg_gated() {
+    let s = S { int: 42 };
+    // Check serialization
+    let s = ::s_json::to_string(&s).unwrap();
+    ::std::assert_eq!(s, r#"{"int":"42"}"#);
+    let s: S = ::s_json::from_str(&s).unwrap();
+    ::std::assert_eq!(s.int, 42);
+}
